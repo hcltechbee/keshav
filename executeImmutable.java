51215ec42fa814503ebdb8ef2d174aebbd7364ba
KESHAV-CHOUDHARY-hub <73520580+KESHAV-CHOUDHARY-hub@users.noreply.github.com>
2020-11-03 21:25:58 +0530

    Create executeImmutable.java

---
+++ executeImmutable.java
@@ -0,0 +1,10 @@
+package practice7;
+
+public class executeImmutable {
+
+	public static void main(String args[])
+	{
+		immutableClass k=new immutableClass(86);
+		System.out.println(k.displayMarks());
+	}
+}
