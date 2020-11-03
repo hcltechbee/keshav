f723a266b01fe1a8d3affd5d12c6c9c30f1b88af
KESHAV-CHOUDHARY-hub <73520580+KESHAV-CHOUDHARY-hub@users.noreply.github.com>
2020-11-03 21:09:06 +0530

    Create UsingArrayList2.java

---
+++ UsingArrayList2.java
@@ -0,0 +1,32 @@
+package practice3;
+
+import java.util.ArrayList;
+import java.util.*;
+import java.util.Iterator;
+import java.util.List;
+
+public class UsingArrayList {
+	public static void main(String ags[])
+	{
+		List<String> players=new ArrayList<>();            // using arraylist to edit,iterate and print player names
+		players.add("KESHAV");
+		players.add("Rahul");
+		players.add("Aman");
+		players.add("Shubham");
+		System.out.println("PLAYERS ARE ->"+players);
+		players.remove(3);
+		players.add(3,"Zarer");
+		System.out.println("PLAYERS ARE ->"+players);
+		
+		Collections.sort(players);                          // sorting array
+		
+		System.out.println("AFter SORTING");
+		Iterator<String> play=players.iterator();
+		while(play.hasNext())
+		{
+			System.out.println("PLAYER NAME -> "+play.next());
+		}
+		
+	}
+
+}
