d57efa030dfcfb816caa783c1ab5965685bf2d91
KESHAV-CHOUDHARY-hub <73520580+KESHAV-CHOUDHARY-hub@users.noreply.github.com>
2020-11-03 21:26:29 +0530

    Create immutableClass.java

---
+++ immutableClass.java
@@ -0,0 +1,20 @@
+package practice7;
+
+
+
+public final class immutableClass {
+
+	final int KeshavMarks;
+	
+	public immutableClass(int marks)
+	{
+		this.KeshavMarks = marks;
+	}
+	
+	public int displayMarks()
+	{
+		return KeshavMarks;
+	}
+
+}
+
