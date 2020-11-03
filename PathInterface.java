b2e778826a14028caf19f421e742ad055e9cad95
KESHAV-CHOUDHARY-hub <73520580+KESHAV-CHOUDHARY-hub@users.noreply.github.com>
2020-11-03 21:22:37 +0530

    Create PathInterface.java

---
+++ PathInterface.java
@@ -0,0 +1,43 @@
+  package practice6;
+
+import java.io.*;
+import java.nio.*;
+import java.nio.file.*;
+
+public class PathInterface {
+   public static void main(String[] args) throws IOException {
+      Path path = Paths.get("C:\\Users\\hppc\\Desktop\\ex.txt");
+      FileSystem fs =  path.getFileSystem();
+      
+      System.out.println(fs.toString());
+      
+      System.out.println(path.isAbsolute());
+      
+      System.out.println(path.getFileName());
+      
+      System.out.println(path.toAbsolutePath().toString());
+      
+      System.out.println(path.getRoot());
+      
+      System.out.println(path.getParent());
+      
+      System.out.println(path.getNameCount());
+      
+      System.out.println(path.getName(0));
+      
+      System.out.println(path.subpath(0, 2));
+      
+      System.out.println(path.toString());
+      
+      System.out.println(path.getNameCount());
+      
+      Path realPath = path.toRealPath(LinkOption.NOFOLLOW_LINKS);
+      
+      System.out.println(realPath.toString());
+      
+      String originalPath = "d:\\keshav\\Desktop\\..\\work\\example.txt";
+      Path path1 = Paths.get(originalPath);
+      Path path2 = path1.normalize();
+      System.out.println("path2 = " + path2);
+   }
+}
