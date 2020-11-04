f00f16a2277a033b878be86085aebdffc6f55363
KESHAV-CHOUDHARY-hub <73520580+KESHAV-CHOUDHARY-hub@users.noreply.github.com>
2020-11-04 09:10:04 +0530

    Create deletionCatcher.java

---
+++ UserManagement/deletionCatcher.java
@@ -0,0 +1,12 @@
+package com.javaClass.deletion;
+
+public class DeletionCatcher {
+	public static String checkDeletion(int checkExecution)
+	{
+		if(checkExecution==1) {
+			return "TASK PERFORMED";
+		} else {
+			return "TASK NOT PERFORMED";
+		}
+	}
+}
