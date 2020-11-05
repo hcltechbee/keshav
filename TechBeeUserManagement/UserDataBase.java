ba547d35d3d45d9b1bbf3689c68a1f069f48db4a
KESHAV-CHOUDHARY-hub <73520580+KESHAV-CHOUDHARY-hub@users.noreply.github.com>
2020-11-05 15:10:00 +0530

    Update UserDataBase.java

--- TechBeeUserManagement/UserDataBase.java
+++ TechBeeUserManagement/UserDataBase.java
@@ -4,51 +4,93 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import constants.QueryConstants;
+import constants.URLConstants;
+
 public class UsersDataBase {
+	
+	/*-----------------------------------------------------------------THIS PART IS FOR CREATING CONNECTION WITH EMPLOYEES---------*/
+	
 	public static Connection getConnection(){  
-        Connection con=null;  
+		URLConstants DBConnectivity= new URLConstants();  // Creating instance of Constant class for DB Connectivity
         try{  
-            Class.forName("com.mysql.jdbc.Driver");  
-            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/keshavdb","root","Keshav@2313");  
+            Class.forName(DBConnectivity.dataBaseDriver);
+            DBConnectivity.dataBaseConnection=DriverManager.getConnection(URLConstants.dataBaseURL, URLConstants.dataBaseUserName, URLConstants.dataBasePassword);  
         }catch(Exception e){System.out.println(e);}  
-        return con;  
+        return DBConnectivity.dataBaseConnection;  
     }  
+	
+	/*-----------------------------------------------------------------THIS PART IS FOR GEETING ALL EMPLOYEES--------------*/
 	
 	 public static List<Users> getAllEmployees(){  
 	        List<Users> list=new ArrayList<>();  
+	        URLConstants DBConnectivity= new URLConstants();  // Creating instance of Constant class for DB Connectivity
+	        QueryConstants queries = new QueryConstants();
 	        
 	try{  
-        Connection con=UsersDataBase.getConnection();  
-        PreparedStatement ps=con.prepareStatement("select * from user905");  
-        ResultSet rs=ps.executeQuery();  
-        while(rs.next()){  
+        DBConnectivity.dataBaseConnection=UsersDataBase.getConnection();  
+        PreparedStatement preparedStatement=DBConnectivity.dataBaseConnection.prepareStatement(queries.displayAllUsers);  
+        ResultSet resultSet=preparedStatement.executeQuery();  
+        while(resultSet.next()){  
             Users e=new Users();  
-            e.setId(rs.getInt(1));  
-            e.setName(rs.getString(2));  
-            e.setPassword(rs.getString(3));  
-            e.setEmail(rs.getString(4));  
-            e.setCountry(rs.getString(5));  
+            e.setId(resultSet.getInt(1));  
+            e.setName(resultSet.getString(2));  
+            e.setPassword(resultSet.getString(3));  
+            e.setEmail(resultSet.getString(4));  
+            e.setCountry(resultSet.getString(5));  
             list.add(e);  
         }  
-        con.close();  
+        DBConnectivity.dataBaseConnection.close();  
     }catch(Exception e){e.printStackTrace();}  
       
     return list;  
 	 }  
 
+	 /*-----------------------------------------------------------------THIS PART IS FOR DELETING USERS FROM DB--------------*/
 	 
 	  public static int deleteUser(int id){  
 	        int status=0;  
+	        URLConstants DBConnectivity= new URLConstants();  // Creating instance of Constant class for DB Connectivity
+	        QueryConstants queries = new QueryConstants();
 	        try{  
-	            Connection con=UsersDataBase.getConnection();  
-	            PreparedStatement ps=con.prepareStatement("delete from user905 where id=?");  
-	            ps.setInt(1,id);  
-	            status=ps.executeUpdate();  
+	            DBConnectivity.dataBaseConnection=UsersDataBase.getConnection();  
+	            PreparedStatement preparedStatement=DBConnectivity.dataBaseConnection.prepareStatement(queries.deleteUsers);  
+	            preparedStatement.setInt(1,id);  
+	            status=preparedStatement.executeUpdate();  
 	              
-	            con.close();  
+	            DBConnectivity.dataBaseConnection.close();  
 	        }catch(Exception e){e.printStackTrace();}  
 	          
 	        return status;  
 	    }  
-
+	
+	  
+	  
+	  /*-----------------------------------------------------------------THIS PART IS FOR SEARCHING EMPLOYEES--------------*/
+	  
+	  
+		 public static List<Users> getAllEmployees(String userName){  
+		        List<Users> filterList=new ArrayList<>();  
+		       // userName="%"+userName+"%";
+		        URLConstants DBConnectivity= new URLConstants();  // Creating instance of Constant class for DB Connectivity
+		        QueryConstants queries = new QueryConstants();
+		try{  
+	        DBConnectivity.dataBaseConnection=UsersDataBase.getConnection();  
+	        PreparedStatement preparedStatement=DBConnectivity.dataBaseConnection.prepareStatement(queries.searchUser);  
+	        preparedStatement.setString(1,userName); 
+	        ResultSet resultSet=preparedStatement.executeQuery();  
+	        while(resultSet.next()){  
+	            Users e=new Users();  
+	            e.setId(resultSet.getInt(1));  
+	            e.setName(resultSet.getString(2));  
+	            e.setPassword(resultSet.getString(3));  
+	            e.setEmail(resultSet.getString(4));  
+	            e.setCountry(resultSet.getString(5));  
+	            filterList.add(e);  
+	        }  
+	        DBConnectivity.dataBaseConnection.close();  
+	    }catch(Exception e){e.printStackTrace();}  
+	      
+	    return filterList;  
+	 } 
 }
