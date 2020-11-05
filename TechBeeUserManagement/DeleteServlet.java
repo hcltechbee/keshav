5fee492a36580646151019b90ffeaba8bc992d83
KESHAV-CHOUDHARY-hub <73520580+KESHAV-CHOUDHARY-hub@users.noreply.github.com>
2020-11-05 15:07:32 +0530

    Update DeleteServlet.java

--- TechBeeUserManagement/DeleteServlet.java
+++ TechBeeUserManagement/DeleteServlet.java
@@ -2,12 +2,12 @@ package com.servlet.users;
 
 import java.io.IOException;  
 import javax.servlet.ServletException;  
-import javax.servlet.annotation.WebServlet;  
 import javax.servlet.http.HttpServlet;  
 import javax.servlet.http.HttpServletRequest;  
 import javax.servlet.http.HttpServletResponse;
 import com.classes.users.UsersDataBase;  
   
+@SuppressWarnings("serial")
 public class DeleteServlet extends HttpServlet {  
     protected void doGet(HttpServletRequest request, HttpServletResponse response)   
              throws ServletException, IOException {  
