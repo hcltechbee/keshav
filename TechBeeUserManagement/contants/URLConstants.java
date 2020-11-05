91299356a544cb8c02a092f548451e679efb2e9f
KESHAV-CHOUDHARY-hub <73520580+KESHAV-CHOUDHARY-hub@users.noreply.github.com>
2020-11-05 15:13:02 +0530

    Create URLConstants.java

---
+++ TechBeeUserManagement/contants/URLConstants.java
@@ -0,0 +1,11 @@
+package constants;
+
+import java.sql.*;
+
+public class URLConstants {		
+	public static String dataBaseURL="jdbc:mysql://localhost:3306/keshavdb";
+	public static String dataBaseUserName="root";
+	public static String dataBasePassword="Keshav@2313";
+	public final String dataBaseDriver="com.mysql.jdbc.Driver";
+	public Connection dataBaseConnection=null; 
+}
